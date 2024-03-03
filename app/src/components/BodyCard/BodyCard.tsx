6e2e747ccaac20fc3e942302648e937a2cf5d0c4
sverg84 <134344913+sverg84@users.noreply.github.com>
2024-03-02 17:58:09 -0800

    no hash link scrolling for now

--- app/src/components/BodyCard/BodyCard.tsx
+++ app/src/components/BodyCard/BodyCard.tsx
@@ -1,5 +1,4 @@
 import { Card } from "@mantine/core";
-import { useLocation } from "@remix-run/react";
 import type { AboutMeCardFooterType } from "components/about/AboutMeCardFooter";
 import * as React from "react";
 
@@ -13,18 +12,8 @@ type Props = Readonly<{
 }>;
 
 export default function BodyCard({ children, footer, id, title }: Props) {
-  const ref = React.useRef<HTMLLIElement>(null);
-
-  const { hash } = useLocation();
-
-  React.useEffect(() => {
-    if (hash.slice(1) === id && ref.current != null) {
-      ref.current.scrollIntoView();
-    }
-  }, [hash, id]);
-
   return (
-    <Card component="li" padding="xl" withBorder={true}>
+    <Card component="li" id={id} padding="xl" withBorder={true}>
       {title && <BodyCardHeader title={title} />}
       <Card.Section px="xl" py="md" withBorder={true}>
         {children}
