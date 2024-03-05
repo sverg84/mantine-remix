39e8338e8552e40626772df7d2177c90d216a66f
sverg84 <134344913+sverg84@users.noreply.github.com>
2024-03-04 16:17:26 -0800

    why was i still console logging

--- app/src/components/about/AboutMeCard.tsx
+++ app/src/components/about/AboutMeCard.tsx
@@ -1,12 +1,8 @@
-import { useMantineTheme } from "@mantine/core";
 import BodyCard from "components/BodyCard/BodyCard";
 
 import AboutMeCardBody from "./AboutMeCardBody";
 
 export default function AboutMeCard() {
-  const theme = useMantineTheme();
-  console.log(theme.breakpoints);
-
   return (
     <BodyCard id="about">
       <AboutMeCardBody />
